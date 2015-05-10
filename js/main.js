8658b0e0512131eaa66e8ca2aba2c72454835eec
soru13 <jesus.ed13@gmail.com>
2015-05-10 00:43:08 -0600

    add imagen y sitio

---
+++ js/main.js
@@ -0,0 +1,18 @@
+$(document).on('ready',function(){
+    $(".fancy_title").lettering();
+    $('.tlt').textillate();
+    $(function() {
+	  $('a[href*=#]:not([href=#])').click(function() {
+	    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+	      var target = $(this.hash);
+	      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+	      if (target.length) {
+	        $('html,body').animate({
+	          scrollTop: target.offset().top
+	        }, 1000);
+	        return false;
+	      }
+	    }
+	  });
+	});
+});
